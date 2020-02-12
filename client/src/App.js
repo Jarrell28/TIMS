aaf0d7a462de4a624e570f68520666c5228baa85
Melissa <melgenao8@gmail.com>
2020-02-11 19:07:59 -0600

    Updated Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import MainNav from './components/Nav';
 import Carousel from './components/Carousel';
 
-function App() {
-  return (
-    <div className="App">
+
+class App extends Component {
+
+  render() {
+  return(
+    <div className = "App" >
       <MainNav />
       <Carousel />
     </div>
   );
+  }
 }
 
 export default App;
