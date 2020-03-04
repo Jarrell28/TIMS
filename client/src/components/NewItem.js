8bb60dd4fdc4b824869aeab02d1d14d74d70d29f
Melissa <melgenao8@gmail.com>
2020-03-03 21:16:08 -0600

    Styled pop up modals. Changed *available* font under carousel headlines

--- client/src/components/NewItem.js
+++ client/src/components/NewItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "../css/main.css";
+import "../css/newItem.css";
 
 // This is the ADD NEW PRODUCT Module Component
 const NewItem = (props) => {
@@ -7,23 +8,32 @@ const NewItem = (props) => {
     const renderInputs = props.inputNames.map(input => {
         return (
             <div className="form-group" key={input.dbName + "-id"}>
-                <label htmlFor={input.dbName}>{input.displayName}</label>
+                <label htmlFor={input.dbName} className={"newItemLabel"}>{input.displayName}</label>
+                {/* boxes here */}
                 <input className="form-control" type="text" name={input.dbName} id={input.dbName} placeholder={"Enter " + input.displayName} />
             </div>
         )
     })
 
     return (
+
         <form onSubmit={props.handleFormSubmit} encType="multipart/form-data" style={{ overflow: "auto" }}>
-            <div className="d-flex justify-content-between">
-                <h2>Add New Equipment</h2>
-                <span onClick={props.toggleNewItem}>Close</span>
+            <div className="bg">
+                <div className="d-flex justify-content-between">
+
+                    <div className="header">Add New Equipment</div>
+                    <span onClick={props.toggleNewItem}>Close</span>
+                </div>
+
+
+                {renderInputs}
+                {props.children}
+
+                {/* This button is inside module pop up */}
+                <input className="add-button" type="submit" value="Add Item" />
             </div>
-            {renderInputs}
-            {props.children}
-            {/* This button is inside module pop up */}
-            <input className="add-button" type="submit" value="Add Item" />
         </form>
+
     )
 }
 
