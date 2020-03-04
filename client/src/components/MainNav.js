dec406d6c888ceb91d22df870a98f715395580ad
Jessica Ibarra <jess19ibarra@gmail.com>
2020-03-03 21:22:14 -0600

    fixed profile page

--- client/src/components/MainNav.js
+++ client/src/components/MainNav.js
@@ -1,87 +1,109 @@
-import React from 'react';
-import SideNav, { NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
-import { Link } from 'react-router-dom';
+import React from "react";
+import SideNav, { NavItem, NavIcon, NavText } from "@trendmicro/react-sidenav";
+import { Link } from "react-router-dom";
 
-import '../css/nav.css';
+import "../css/nav.css";
 // import logo from '../images/TIMS-logo-06.svg';
 
 // Be sure to include styles at some point, probably during your bootstraping
-import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+import "@trendmicro/react-sidenav/dist/react-sidenav.css";
 
-const MainNav = (props) => {
-    return (
-        <SideNav
-        className= "navShadow"
-            onSelect={(selected) => {
-                // Add your code here
-            }}
-        >
-            <SideNav.Toggle />
-            <SideNav.Nav defaultSelected="products">
+const MainNav = props => {
+  return (
+    <SideNav
+      className="navShadow"
+      onSelect={selected => {
+        // Add your code here
+      }}
+    >
+      <SideNav.Toggle />
+      <SideNav.Nav defaultSelected="products">
+        <Link to="/">
+          <img
+            src="images/TIMS-logo-06.svg"
+            className="navLogo"
+            alt="title or description"
+          />
+        </Link>
 
-                <Link to="/"><img src="images/TIMS-logo-06.svg" className="navLogo" alt='title or description' /></Link>
+        {/* PRODUCT SEARCH TAB */}
+        {/* LINK WITH DROP DOWN*/}
+        <NavItem eventKey="products">
+          <NavIcon>
+            <Link to="/">
+              {" "}
+              <i className="fa fa-search" style={{ fontSize: "1.75em" }} />
+            </Link>
+          </NavIcon>
+          <NavText>
+            <Link to="/">Product Search</Link>
+          </NavText>
 
-                {/* PRODUCT SEARCH TAB */}
-                {/* LINK WITH DROP DOWN*/}
-                <NavItem eventKey="products">
-                    <NavIcon>
-                        <Link to="/"> <i className="fa fa-search" style={{ fontSize: '1.75em' }} /></Link>
-                    </NavIcon>
-                    <NavText><Link to="/">
-                        Product Search
-                        </Link></NavText>
+          {props.mainNav.map((nav, index) => {
+            return (
+              <NavItem
+                key={"mainNav_Context_" + index}
+                eventKey="products/barchart"
+              >
+                <NavText onClick={() => props.onContextClick(nav.destination)}>
+                  {nav.title}
+                </NavText>
+              </NavItem>
+            );
+          })}
+        </NavItem>
 
-                    {props.mainNav.map((nav, index) => {
-                        return <NavItem key={"mainNav_Context_" + index} eventKey="products/barchart">
-                            <NavText onClick={() => props.onContextClick(nav.destination)}>
-                                {nav.title}
-                            </NavText>
-                        </NavItem>
-                    })}
+        {/* LAPTOP LOANERS (SINGLE LINK) */}
+        <NavItem eventKey="laptop">
+          <NavIcon>
+            <Link to="/loaners">
+              {" "}
+              <i
+                className="fa fa-laptop"
+                style={{ color: "#fff", fontSize: "1.75em" }}
+              />
+            </Link>
+          </NavIcon>
+          <NavText className="subTitle">
+            <Link to="/loaners">Laptop Checkout</Link>
+          </NavText>
+          <NavItem eventKey="products/loaners-mac">
+            <NavText>Mac</NavText>
+          </NavItem>
+          <NavItem eventKey="products/loaners-windows">
+            <NavText>Windows</NavText>
+          </NavItem>
+        </NavItem>
 
-                </NavItem>
+        {/* YOUR HISTORY (SINGLE LINK) */}
+        <NavItem eventKey="user">
+          <NavIcon>
+            <Link to="/profile">
+              {" "}
+              <i
+                className="fa fa-user"
+                style={{ color: "#fff", fontSize: "1.75em" }}
+              />
+            </Link>
+          </NavIcon>
+          <NavText className="subTitle">
+            <Link to="/profile">Profile</Link>
+          </NavText>
+        </NavItem>
 
-
-                {/* LAPTOP LOANERS (SINGLE LINK) */}
-                <NavItem eventKey="laptop">
-                    <NavIcon>
-                        <Link to="/loaners"> <i className="fa fa-laptop" style={{ color: "#fff", fontSize: '1.75em' }} /></Link>
-                    </NavIcon>
-                    <NavText className="subTitle"><Link to="/loaners">Laptop Checkout</Link></NavText>
-                    <NavItem eventKey="products/loaners-mac">
-                        <NavText>Mac</NavText>
-                    </NavItem>
-                    <NavItem eventKey="products/loaners-windows">
-                        <NavText>Windows</NavText>
-                    </NavItem>
-                </NavItem>
-
-
-                {/* YOUR HISTORY (SINGLE LINK) */}
-                <NavItem eventKey="history">
-                    <NavIcon><i className="fa fa-history" style={{ color: "#fff", fontSize: '1.75em' }} /></NavIcon>
-                    <NavText>Your History</NavText>
-                </NavItem>
-
-
-
-                {/* LOGOUT (SINGLE LINK) */}
-                <NavItem eventKey="logOut">
-                    <NavIcon>
-                        <i className="fa fa-sign-out" style={{ color: "#fff", fontSize: '1.75em' }} />
-                    </NavIcon>
-                    <NavText className="subTitle">Logout</NavText>
-                </NavItem>
-
-            </SideNav.Nav>
-        </SideNav >
-    )
-}
-
-
-
-
-
-
+        {/* LOGOUT (SINGLE LINK) */}
+        <NavItem eventKey="logOut">
+          <NavIcon>
+            <i
+              className="fa fa-sign-out"
+              style={{ color: "#fff", fontSize: "1.75em" }}
+            />
+          </NavIcon>
+          <NavText className="subTitle">Logout</NavText>
+        </NavItem>
+      </SideNav.Nav>
+    </SideNav>
+  );
+};
 
 export default MainNav;
---
+++ client/src/pages/Profile.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import Image from "../images/peter.png";
+import MainNav from "../components/MainNav";
+import jwt_decode from "jwt-decode";
+
+class Profile extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      errors: {}
+    };
+  }
+
+  componentDidMount() {
+    const token = localStorage.usertoken;
+    if (token) {
+      const decoded = jwt_decode(token);
+      this.setState({
+        name: decoded.name,
+        email: decoded.email,
+        role: decoded.role
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <MainNav
+          onContextClick={this.props.onContextClick}
+          mainNav={this.props.mainNav}
+          productContext={this.props.productContext}
+        />
+        <div className="container mt-4">
+          <div className="profile-card text-center">
+            <img src={Image} alt="profile" width="250" height="250" />
+            <h1 className="mt-4">
+              Name: {this.state.name ? this.state.name : "James"}
+            </h1>
+            <p class="h2 my-3">
+              Email: {this.state.email ? this.state.email : "jterrell@test.com"}
+            </p>
+            <p class="h2 my-3">
+              Role: {this.state.role ? this.state.role : "Technician"}
+            </p>
+                
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Profile;
